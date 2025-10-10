d5e1aa338241646a8ed3d31cf93dd6bd5136dadc
ZainAkuji <zainul_a@hotmail.co.uk>
2025-10-10 18:17:24 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -309,7 +309,7 @@ export default function App(){
         </div>
       </section>
 
-     <div className="mx-auto max-w-6xl px-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-5 justify-items-center">
+     <div className="mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-5">
         {GROUPED
           .filter(g => {
             const q = (query || "").toLowerCase();
