d485e71643b4d114bb3c1167725c1cae60bac545
ZainAkuji <zainul_a@hotmail.co.uk>
2025-11-03 15:21:50 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -117,13 +117,13 @@ function AboutSection() {
       style={{
         backgroundImage: "linear-gradient(rgba(0,0,0,0.40), rgba(0,0,0,0.40)), url('/about_bg.png')",
         backgroundSize: "cover",
-        backgroundPosition: "70% center",
+        backgroundPosition: "90% center",
         backgroundRepeat: "no-repeat",
       }}
     >
       
       
-      <div className="max-w-6xl px-1">
+      <div className="max-w-6xl px-4">
         <h2 className="text-2xl md:text-3xl font-bold text-white">
           About Yoghurt of Youth
         </h2>
