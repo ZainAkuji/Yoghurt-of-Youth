81201dc3a7a539c17f58766e8f9f27056eb3d945
ZainAkuji <zainul_a@hotmail.co.uk>
2025-12-13 16:59:13 +0000

    Update capture-order.ts

--- api/paypal/capture-order.ts
+++ api/paypal/capture-order.ts
@@ -1,1 +1,32 @@
+export default async function handler(req, res) {
+  const { orderId } = req.body;
+
+  const r = await fetch(
+    `https://api-m.paypal.com/v2/checkout/orders/${orderId}/capture`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Basic " +
+          Buffer.from(`${process.env.PAYPAL_CLIENT_ID}:${process.env.PAYPAL_SECRET}`).toString("base64"),
+      },
+    }
+  );
+
+  const data = await r.json();
+
+  // Email from server
+  if (data.status === "COMPLETED") {
+    await sendEmail({
+      name: data.payer.name.given_name,
+      email: data.payer.email_address,
+      orderId,
+      total: data.purchase_units[0].amount.value,
+      paymentMethod: "PayPal",
+    });
+  }
+
+  res.status(200).json(data);
+}
 
