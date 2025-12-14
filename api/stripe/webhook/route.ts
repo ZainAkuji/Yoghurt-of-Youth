fd214f19e5a3f3059af999f95062ea89c99a15fd
ZainAkuji <zainul_a@hotmail.co.uk>
2025-12-14 21:57:10 +0000

    Update route.ts

--- api/stripe/webhook/route.ts
+++ api/stripe/webhook/route.ts
@@ -3,9 +3,7 @@ import Stripe from "stripe";
 export const runtime = "nodejs"; // important for Stripe lib
 export const dynamic = "force-dynamic";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2024-06-20",
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 async function sendEmailJS(templateParams: Record<string, any>) {
   const res = await fetch("https://api.emailjs.com/api/v1.0/email/send", {
