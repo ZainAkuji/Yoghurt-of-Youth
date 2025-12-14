be203518a176bbff366398fa2408270c35369e31
ZainAkuji <zainul_a@hotmail.co.uk>
2025-12-14 21:55:10 +0000

    Update verify-session.ts

--- api/stripe/verify-session.ts
+++ api/stripe/verify-session.ts
@@ -1,4 +1,4 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
