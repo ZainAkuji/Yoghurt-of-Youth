b8776b666ea59ded8955142890b3ba506a11aad7
ZainAkuji <zainul_a@hotmail.co.uk>
2025-12-20 16:40:02 +0000

    Update create-subscription-session.ts

--- api/stripe/create-subscription-session.ts
+++ api/stripe/create-subscription-session.ts
@@ -1,7 +1,7 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string,
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 function getPriceId(planKey: string) {
   const map: Record<string, string | undefined> = {
