b004f73fa3cf10ba481f9f3e23e90ae2a42e6fa0
ZainAkuji <zainul_a@hotmail.co.uk>
2025-12-27 11:55:47 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2323,7 +2323,7 @@ function PayModal({
           className="rounded-2xl px-5 py-3 text-sm font-semibold bg-white text-slate-900 hover:bg-amber-300 transition"
         >
           <img
-            src="/stripe_button.jpg"
+            src="/stripe_button.jpeg"
             alt="Pay with Stripe"
             className="h-6 w-auto"
           />
@@ -2402,7 +2402,7 @@ function PayModal({
           className="rounded-2xl px-5 py-3 text-sm font-semibold bg-[#ffc439] text-slate-900 hover:bg-[#ffcf43] transition"
         >
           <img
-            src="/paypal_button.jpg"
+            src="/paypal_button.jpeg"
             alt="Pay with PayPal"
             className="h-6 w-auto"
            />
