1185368623983f5f5bf9e14f978b78b279d852f4
ZainAkuji <zainul_a@hotmail.co.uk>
2025-12-27 16:19:03 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2320,7 +2320,7 @@ function PayModal({
               setSending(false);
             }
           }}
-          className="sm:w-72 rounded-2xl px-1 text-sm font-semibold text-slate-900 bg-[#635BFF] hover:bg-[#5147ff] transition flex items-center justify-center"
+          className="sm:w-72 h-14 rounded-2xl px-1 text-sm font-semibold text-slate-900 bg-[#635BFF] hover:bg-[#5147ff] transition flex items-center justify-center"
         >
           <img
             src="/stripe_logo.png"
@@ -2399,7 +2399,7 @@ function PayModal({
               setSending(false);
             }
           }}
-          className="sm:w-72 rounded-2xl px-1 py-1 text-sm font-semibold bg-[#ffc439] text-slate-900 hover:bg-[#ffcf43] transition flex items-center justify-center"
+          className="sm:w-72 h-14 rounded-2xl px-1 py-1 text-sm font-semibold bg-[#ffc439] text-slate-900 hover:bg-[#ffcf43] transition flex items-center justify-center"
         >
           <img
             src="/paypal_logo.png"
@@ -2410,7 +2410,7 @@ function PayModal({
 
         <button
           onClick={onClose}
-          className="sm:w-72 rounded-2xl border border-white/30 px-1 py-3 text-sm font-semibold text-white hover:bg-white/10 transition"
+          className="sm:w-72 h-14 rounded-2xl border border-white/30 px-1 py-3 text-sm font-semibold text-white hover:bg-white/10 transition"
         >
           Cancel
         </button>
