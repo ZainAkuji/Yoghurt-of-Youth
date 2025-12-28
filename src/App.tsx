86259affacb3314b9f9bfb15375c30aebf1f4239
ZainAkuji <zainul_a@hotmail.co.uk>
2025-12-28 14:25:41 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1911,7 +1911,7 @@ function PayModal({
             <img
               src="/stripe_logo.png"
               alt="Subscribe with Stripe"
-              className="h-13"
+              className="h-6"
             />
           </button>
           
@@ -2330,7 +2330,7 @@ function PayModal({
           <img
             src="/stripe_logo.png"
             alt="Pay with Stripe"
-            className="h-13"
+            className="h-6"
           />
         </button>
 
