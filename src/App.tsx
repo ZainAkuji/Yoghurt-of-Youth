8688f165cb50b443196f5cb4fc85d94a2ba43576
ZainAkuji <zainul_a@hotmail.co.uk>
2026-02-02 19:12:41 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useMemo, useState } from "react";
 
-const BRAND = "Yoghurt of Youth";
 const OWNER_EMAIL = "zainul_a@hotmail.co.uk";
 const OWNER_PHONE = "+44 7756231844";
 
