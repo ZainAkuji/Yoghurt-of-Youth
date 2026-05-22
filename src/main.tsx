416bf94708f341bdab28a1633d61ded9d3240f60
ZainAkuji <zainul_a@hotmail.co.uk>
2026-05-22 18:25:36 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,31 +3,25 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-// Meta Pixel Setup
-const pixelId = import.meta.env.VITE_META_PIXEL_ID || '1639585477324656';
+// Meta Pixel
+declare global {
+  interface Window { fbq: (...args: any[]) => void; _fbq: any; }
+}
 
-if (typeof window !== 'undefined') {
+const pixelId = import.meta.env.VITE_META_PIXEL_ID;
+
+if (typeof window !== 'undefined' && pixelId) {
   !(function (f: any, b: any, e: any, v: any, n: any, t: any, s: any) {
     if (f.fbq) return;
     n = f.fbq = function () {
       n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
     };
     if (!f._fbq) f._fbq = n;
-    n.push = n;
-    n.loaded = !0;
-    n.version = '2.0';
-    n.queue = [];
-    t = b.createElement(e);
-    t.async = !0;
-    t.src = v;
+    n.push = n; n.loaded = !0; n.version = '2.0'; n.queue = [];
+    t = b.createElement(e); t.async = !0; t.src = v;
     s = b.getElementsByTagName(e)[0];
     s.parentNode?.insertBefore(t, s);
-  })(
-    window,
-    document,
-    'script',
-    'https://connect.facebook.net/en_US/fbevents.js'
-  );
+  })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
 
   window.fbq('init', pixelId);
   window.fbq('track', 'PageView');
