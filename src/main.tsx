aa70d17d2d8bf9b4b08648867f909fde89864f0d
ZainAkuji <zainul_a@hotmail.co.uk>
2026-05-22 20:23:41 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,28 +3,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-// Meta Pixel
 declare global {
   interface Window { fbq: (...args: any[]) => void; _fbq: any; }
-}
-
-const pixelId = import.meta.env.VITE_META_PIXEL_ID;
-
-if (typeof window !== 'undefined' && pixelId) {
-  !(function (f: any, b: any, e: any, v: any, n: any, t: any, s: any) {
-    if (f.fbq) return;
-    n = f.fbq = function () {
-      n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
-    };
-    if (!f._fbq) f._fbq = n;
-    n.push = n; n.loaded = !0; n.version = '2.0'; n.queue = [];
-    t = b.createElement(e); t.async = !0; t.src = v;
-    s = b.getElementsByTagName(e)[0];
-    s.parentNode?.insertBefore(t, s);
-  })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
-
-  window.fbq('init', pixelId);
-  window.fbq('track', 'PageView');
 }
 
 createRoot(document.getElementById('root')!).render(
